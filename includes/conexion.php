f2d382c7058987dbe5099ccb37f157bf15999c6a
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-07-20 15:46:21 -0500

    actualizado2 conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,21 +1,19 @@
 <?php
-// Extraer los datos de la URL de conexión pública
-$url = parse_url(getenv('MYSQL_PUBLIC_URL'));
+$servername = getenv('MYSQLHOST');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$dbname = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-$servername = $url['host'];
-$username = $url['user'];
-$password = $url['pass'];
-$dbname = substr($url['path'], 1); // Elimina la barra inicial del nombre de la base de datos
-$port = $url['port'];
+// Comprobar si las variables están definidas
+if (!$servername || !$username || !$password || !$dbname || !$port) {
+    die('Error: Las variables de entorno para la base de datos no están configuradas correctamente.');
+}
 
-// Depuración para asegurarse de que las variables de entorno están configuradas correctamente
-error_log("Servername: $servername");
-error_log("Username: $username");
-error_log("DB Name: $dbname");
-error_log("Port: $port");
-
+// Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
+// Comprobar si la conexión fue exitosa
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 } else {
