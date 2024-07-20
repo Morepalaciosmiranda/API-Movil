01d5d825c20e83244f41665f0fd1440e3e3bf545
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-07-20 14:57:42 -0500

    Actualizado conexion.php para usar variables de Railway

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,23 +1,11 @@
 <?php
-$database_url = getenv('MYSQL_URL');
+$servername = getenv('MYSQLHOST');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$dbname = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-if ($database_url) {
-    // Si tenemos la URL de Railway, la usamos
-    $url_parts = parse_url($database_url);
-    
-    $servername = $url_parts['host'];
-    $username = $url_parts['user'];
-    $password = $url_parts['pass'];
-    $dbname = ltrim($url_parts['path'], '/');
-} else {
-    // Si no, usamos las variables individuales (por compatibilidad)
-    $servername = getenv('DB_HOST');
-    $username = getenv('DB_USER');
-    $password = getenv('DB_PASSWORD');
-    $dbname = getenv('DB_NAME');
-}
-
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
