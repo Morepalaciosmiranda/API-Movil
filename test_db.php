5d70e9a60c4969ac34915badca57113f67dca00d
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-07-20 21:21:01 -0500

    actualizado login.php

--- test_db.php
+++ test_db.php
@@ -2,13 +2,22 @@
 include 'includes/conexion.php';
 
 echo "<pre>";
-print_r($url);
+print_r(parse_url(getenv("MYSQL_URL")));
 echo "</pre>";
 
 echo "Intentando conectar a la base de datos...<br>";
 
 if ($conn->ping()) {
-    echo "¡Conexión exitosa a la base de datos!";
+    echo "¡Conexión exitosa a la base de datos!<br>";
+    $result = $conn->query("SHOW TABLES");
+    if ($result) {
+        echo "Tablas en la base de datos:<br>";
+        while ($row = $result->fetch_array()) {
+            echo $row[0] . "<br>";
+        }
+    } else {
+        echo "Error al obtener las tablas: " . $conn->error;
+    }
 } else {
     echo "Error de conexión: " . $conn->error;
 }
