b766454e9679544df4a5995e5e6fc1de59c834d5
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-07-21 12:27:11 -0500

    actualizado2 sidebar.php

--- views/sidebar.php
+++ views/sidebar.php
@@ -27,30 +27,57 @@ $modules = array(
     9 => array('id' => '9', 'name' => 'Productos', 'icon' => 'fas fa-box-open', 'link' => './productos.php'),
 );
 ?>
-<div class="sidebar">
-    <h1 class="logo">EXTERMINIO</h1>
-    <div class="profile-div">
-        <p class="user1" onclick="toggleUserOptions()">
-            <i class="fa fa-user"></i> <?php echo isset($_SESSION['correo_electronico']) ? $_SESSION['correo_electronico'] : ''; ?>
-        </p>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/index6.css">
+</head>
+
+<body>
+    <div class="overlay" id="overlay">
+        <img src="../img/hamburguesa-cara-aterradora-otro-rasgo-monstruo-ilustracion-vectorial_648963-489-Photoroom.png" alt="Animación">
     </div>
-    <br><br>
-    <ul class="nav">
-        <li><a href="#" onclick="showOverlayAndRedirect('inicio.php')"><i class="fa fa-home"></i> Inicio</a></li> 
-        <?php foreach ($modules as $module) : ?>
-            <?php if (in_array($module['id'], $permissions)) : ?>
-                <li><a href="#" onclick="showOverlayAndRedirect('<?php echo $module['link']; ?>')"><i class="<?php echo $module['icon']; ?>"></i> <?php echo $module['name']; ?></a></li>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </ul>
-</div>
-<script>
-    function showOverlayAndRedirect(link) {
-        var overlay = document.getElementById('overlay');
-        overlay.style.display = 'flex';
-        overlay.classList.remove('hidden');
-        setTimeout(function() {
-            window.location.href = link;
-        }, 1000); 
-    }
-</script>
+    <div class="sidebar">
+        <h1 class="logo">EXTERMINIO</h1>
+        <div class="profile-div">
+            <p class="user1" onclick="toggleUserOptions()">
+                <i class="fa fa-user"></i> <?php echo isset($_SESSION['correo_electronico']) ? $_SESSION['correo_electronico'] : ''; ?>
+            </p>
+        </div>
+        <br><br>
+        <ul class="nav">
+            <li><a href="#" onclick="showOverlayAndRedirect('inicio.php')"><i class="fa fa-home"></i> Inicio</a></li> 
+            <?php foreach ($modules as $module) : ?>
+                <?php if (in_array($module['id'], $permissions)) : ?>
+                    <li><a href="#" onclick="showOverlayAndRedirect('<?php echo $module['link']; ?>')"><i class="<?php echo $module['icon']; ?>"></i> <?php echo $module['name']; ?></a></li>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </ul>
+        <script>
+            function toggleUserOptions() {
+                var userOptionsContainer = document.getElementById("userOptionsContainer");
+                if (userOptionsContainer.style.display === "none" || userOptionsContainer.style.display === "") {
+                    userOptionsContainer.style.display = "block";
+                } else {
+                    userOptionsContainer.style.display = "none";
+                }
+            }
+
+            function showOverlayAndRedirect(link) {
+                var overlay = document.getElementById('overlay');
+                overlay.style.display = 'flex';
+                overlay.classList.remove('hidden');
+                setTimeout(function() {
+                    window.location.href = link;
+                }, 1000); 
+            }
+        </script>
+    </div>
+</body>
+
+</html>
