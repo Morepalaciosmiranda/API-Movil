7320262b8646da60b0ef3ccc5b0a30b73c8a6c9b
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-07-21 13:17:10 -0500

    actualizado2 sidebar.php

--- controller/obtener_detalles_pedido.php
+++ controller/obtener_detalles_pedido.php
@@ -1,44 +1,55 @@
 <?php
-
-
-
 include_once('../includes/conexion.php');
-
 
 $idPedido = $_GET['idPedido'];
 
-$sql_detalles_pedido = "SELECT * FROM detalle_pedido WHERE id_pedido = '$idPedido'";
-$result_detalles_pedido = mysqli_query($conn, $sql_detalles_pedido);
+// Obtener detalles del pedido
+$sql_detalles_pedido = "SELECT * FROM detalle_pedido WHERE id_pedido = ?";
+$stmt_detalles = mysqli_prepare($conn, $sql_detalles_pedido);
+mysqli_stmt_bind_param($stmt_detalles, "i", $idPedido);
+mysqli_stmt_execute($stmt_detalles);
+$result_detalles_pedido = mysqli_stmt_get_result($stmt_detalles);
 
-
-$sql_cliente = "SELECT nombre, direccion, barrio, telefono FROM detalle_pedido WHERE id_pedido = '$idPedido' LIMIT 1";
-$result_cliente = mysqli_query($conn, $sql_cliente);
+// Obtener datos del cliente
+$sql_cliente = "SELECT p.nombre_cliente, p.direccion, p.barrio, p.telefono 
+                FROM pedidos p 
+                WHERE p.id_pedido = ?";
+$stmt_cliente = mysqli_prepare($conn, $sql_cliente);
+mysqli_stmt_bind_param($stmt_cliente, "i", $idPedido);
+mysqli_stmt_execute($stmt_cliente);
+$result_cliente = mysqli_stmt_get_result($stmt_cliente);
 
 $response = array();
 
 if (mysqli_num_rows($result_detalles_pedido) > 0 && mysqli_num_rows($result_cliente) > 0) {
-  
     $detalles_pedido = "<ul>";
     while ($row_detalles_pedido = mysqli_fetch_assoc($result_detalles_pedido)) {
-        $detalles_pedido .= "<li>ID Producto: " . $row_detalles_pedido['id_producto'] . "</li>";
-        $detalles_pedido .= "<li>Nombre: " . $row_detalles_pedido['nombre'] . "</li>";
-        $detalles_pedido .= "<li>Cantidad: " . $row_detalles_pedido['cantidad'] . "</li>";
-        $detalles_pedido .= "<li>Valor Unitario: " . $row_detalles_pedido['valor_unitario'] . "</li>";
-        $detalles_pedido .= "<li>Subtotal: " . $row_detalles_pedido['subtotal'] . "</li>";
+        $detalles_pedido .= "<li>ID Producto: " . htmlspecialchars($row_detalles_pedido['id_producto']) . "</li>";
+        $detalles_pedido .= "<li>Nombre: " . htmlspecialchars($row_detalles_pedido['nombre']) . "</li>";
+        $detalles_pedido .= "<li>Cantidad: " . htmlspecialchars($row_detalles_pedido['cantidad']) . "</li>";
+        $detalles_pedido .= "<li>Valor Unitario: " . htmlspecialchars($row_detalles_pedido['valor_unitario']) . "</li>";
+        $detalles_pedido .= "<li>Subtotal: " . htmlspecialchars($row_detalles_pedido['subtotal']) . "</li>";
     }
     $detalles_pedido .= "</ul>";
 
-
     $cliente = mysqli_fetch_assoc($result_cliente);
-
+    
     $response['success'] = true;
     $response['detalles'] = $detalles_pedido;
-    $response['cliente'] = $cliente;
+    $response['cliente'] = array(
+        'nombre' => htmlspecialchars($cliente['nombre_cliente']),
+        'direccion' => htmlspecialchars($cliente['direccion']),
+        'barrio' => htmlspecialchars($cliente['barrio']),
+        'telefono' => htmlspecialchars($cliente['telefono'])
+    );
 } else {
- 
     $response['success'] = false;
     $response['message'] = "No hay detalles disponibles para este pedido.";
 }
 
 echo json_encode($response);
+
+mysqli_stmt_close($stmt_detalles);
+mysqli_stmt_close($stmt_cliente);
+mysqli_close($conn);
 ?>
