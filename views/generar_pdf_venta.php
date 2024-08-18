136d80f1cc6a5dd3820814ad56c98a92fc40b5ee
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-08-18 13:15:02 -0500

    actualizado

--- views/generar_pdf_venta.php
+++ views/generar_pdf_venta.php
@@ -2,26 +2,22 @@
 require_once __DIR__ . '/../libraries/tcpdf/tcpdf.php';
 include_once "../includes/conexion.php";
 
-// Verificar que se ha proporcionado un ID de venta
-if (!isset($_GET['id']) || empty($_GET['id'])) {
-    die('Se requiere un ID de venta válido');
-}
-
-$id_venta = $_GET['id'];
-
-// Obtener los datos de la venta
+// Obtener la venta más reciente
 $sql = "SELECT v.*, u.nombre_usuario, p.nombre_producto, dv.cantidad, dv.precio_unitario
         FROM ventas v
         JOIN usuarios u ON v.id_usuario = u.id_usuario
         JOIN detalle_venta dv ON v.id_venta = dv.id_venta
         JOIN productos p ON dv.id_producto = p.id_producto
-        WHERE v.id_venta = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $id_venta);
-$stmt->execute();
-$result = $stmt->get_result();
+        ORDER BY v.fecha_venta DESC
+        LIMIT 1";
+$result = $conn->query($sql);
 
-// Crear PDF
+if ($result->num_rows == 0) {
+    die('No se encontraron ventas');
+}
+
+$venta = $result->fetch_assoc();
+
 class MYPDF extends TCPDF {
     public function Header() {
         $image_file = __DIR__ . '/../img/LogoExterminio.png';
@@ -45,8 +41,6 @@ $pdf->SetTitle('Factura de Venta');
 $pdf->SetSubject('Factura');
 $pdf->SetKeywords('Factura, Venta, PDF');
 
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
-
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
@@ -64,53 +58,43 @@ $pdf->AddPage();
 
 $pdf->SetFont('helvetica', '', 12);
 
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    
-    $html = '<h2>Detalles de la Venta</h2>';
-    $html .= '<table border="1" cellpadding="5">
-        <tr>
-            <th><b>ID Venta:</b></th>
-            <td>' . $row['id_venta'] . '</td>
-            <th><b>Fecha:</b></th>
-            <td>' . $row['fecha_venta'] . '</td>
-        </tr>
-        <tr>
-            <th><b>Cliente:</b></th>
-            <td colspan="3">' . $row['nombre_usuario'] . '</td>
-        </tr>
-    </table><br><br>';
-    
-    $html .= '<h3>Productos</h3>';
-    $html .= '<table border="1" cellpadding="5">
-        <tr>
-            <th><b>Producto</b></th>
-            <th><b>Cantidad</b></th>
-            <th><b>Precio Unitario</b></th>
-            <th><b>Subtotal</b></th>
-        </tr>';
-    
-    $total = 0;
-    do {
-        $subtotal = $row['cantidad'] * $row['precio_unitario'];
-        $total += $subtotal;
-        $html .= '<tr>
-            <td>' . $row['nombre_producto'] . '</td>
-            <td>' . $row['cantidad'] . '</td>
-            <td>$' . number_format($row['precio_unitario'], 2) . '</td>
-            <td>$' . number_format($subtotal, 2) . '</td>
-        </tr>';
-    } while ($row = $result->fetch_assoc());
-    
-    $html .= '<tr>
-        <th colspan="3" align="right"><b>Total:</b></th>
-        <td><b>$' . number_format($total, 2) . '</b></td>
+$html = '<h2>Detalles de la Venta</h2>';
+$html .= '<table border="1" cellpadding="5">
+    <tr>
+        <th><b>ID Venta:</b></th>
+        <td>' . $venta['id_venta'] . '</td>
+        <th><b>Fecha:</b></th>
+        <td>' . $venta['fecha_venta'] . '</td>
+    </tr>
+    <tr>
+        <th><b>Cliente:</b></th>
+        <td colspan="3">' . $venta['nombre_usuario'] . '</td>
+    </tr>
+</table><br><br>';
+
+$html .= '<h3>Productos</h3>';
+$html .= '<table border="1" cellpadding="5">
+    <tr>
+        <th><b>Producto</b></th>
+        <th><b>Cantidad</b></th>
+        <th><b>Precio Unitario</b></th>
+        <th><b>Subtotal</b></th>
     </tr>';
-    $html .= '</table>';
-    
-    $pdf->writeHTML($html, true, false, true, false, '');
-    
-    $pdf->Output('factura_venta_' . $id_venta . '.pdf', 'I');
-} else {
-    die('No se encontró la venta especificada');
-}
+
+$subtotal = $venta['cantidad'] * $venta['precio_unitario'];
+$html .= '<tr>
+    <td>' . $venta['nombre_producto'] . '</td>
+    <td>' . $venta['cantidad'] . '</td>
+    <td>$' . number_format($venta['precio_unitario'], 2) . '</td>
+    <td>$' . number_format($subtotal, 2) . '</td>
+</tr>';
+
+$html .= '<tr>
+    <th colspan="3" align="right"><b>Total:</b></th>
+    <td><b>$' . number_format($subtotal, 2) . '</b></td>
+</tr>';
+$html .= '</table>';
+
+$pdf->writeHTML($html, true, false, true, false, '');
+
+$pdf->Output('factura_venta_' . $venta['id_venta'] . '.pdf', 'I');
