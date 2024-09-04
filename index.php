3a41e12bcbd44b44779567ba9b5c17ea2bad6a57
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-09-04 11:34:04 -0500

    actualizado

--- index.php
+++ index.php
@@ -161,9 +161,11 @@ if ($_SESSION['rol'] !== 'Usuario') {
                 <div class="content-footer">
                     <div class="contact">
                         <h4>REDES SOCIALES</h4>
-                        <a href="#" class="whatsapp-icon">
-                            <ion-icon name="logo-whatsapp"></ion-icon>
-                        </a>
+                        <div class="social-icons">
+                            <a href="#" class="whatsapp-icon">
+                                <ion-icon name="logo-whatsapp"></ion-icon>
+                            </a>
+                        </div>
                     </div>
                     <div class="group">
                         <p>Disfruta de nuestras deliciosas comidas rápidas preparadas con los mejores ingredientes. Desde hamburguesas jugosas hasta crujientes papas fritas, tenemos todo lo que necesitas para satisfacer tus antojos. ¡Haz tu pedido ahora y experimenta el sabor del auténtico fast food!</p>
