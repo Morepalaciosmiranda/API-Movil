8f521b9b2e2673a59c64f127e3176c28b9ae04ae
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-09-05 20:45:11 -0500

    actualizado

--- controller/compras_controller.php
+++ controller/compras_controller.php
@@ -151,7 +151,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['edit_id_compra'])) {
     exit();
 }
 
-$$consulta_compras = "SELECT c.id_compra, p.nombre_proveedor, i.nombre_insumo, c.marca, c.cantidad, c.fecha_compra, c.total_compra 
+$consulta_compras = "SELECT c.id_compra, p.nombre_proveedor, i.nombre_insumo, c.marca, c.cantidad, c.fecha_compra, c.total_compra 
 FROM compras c
 JOIN proveedores p ON c.id_proveedor = p.id_proveedor
 JOIN insumos i ON c.id_insumo = i.id_insumo
