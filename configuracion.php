7c6d576f93e3746104f4478e620cd96eacdffe54
morepalaciosmiranda <morepalaciosmiranda@gmail.com>
2024-09-14 09:13:03 -0500

    actualizado

--- configuracion.php
+++ configuracion.php
@@ -106,6 +106,7 @@ if (isset($_GET['error'])) {
 
 <body>
     <nav class="navbar">
+        <a href="index.php" class="flecha-retorno"><i class="fas fa-arrow-left"></i></a>
         <div class="logo">
             <img src="./img/LogoExterminio.png" alt="Logo">
         </div>
